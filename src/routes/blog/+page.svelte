4e5a7d0a1570d17ee832baca683ef914f00b0beb
David Brackbill <dmbrackbill@gmail.com>
2024-03-12 12:32:26 -0700

    Blog - Use tailwind classes

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,8 +4,8 @@
 
 <div class="flex items-start w-fit m-auto pb-4">
 
-    <div class="sticky mt-28 top-0">
-        <ul class="nav-text">
+    <div class="sticky mt-24 top-0">
+        <ul class="monospace w-12 justify-start">
             <li><a href="#2024">2024</a></li>
             <li><a href="#2023">2023</a></li>
         </ul>
@@ -14,22 +14,22 @@
     <div><!--Align splash and articles as siblings-->
 
    <!--Splash-->
-    <div class="pl-6">
+    <div class="pl-6 mb-4">
         <h1>Writing on the technical spectrum</h1>
         <p>I do technical blogging about Python, Postgres, and distributed systems. I also post relevant tidbits about the learning process for systems programming and UI frameworks like Svelte.</p>
     </div>
 
    <!--Articles-->
-    <div class="pl-6 article-border">
+    <div class="pl-6 border-l-1-w">
         <h2 id="2024">2024</h2>
-        <div>
+        <div class="article">
             <a href="{base}/psql_poly">Polymorphic functions in Postgres</a>
             <time datetime="2024-02-29">24.02.29</time>
             <p>Intro to polymorphism / What's different in postgres / Why they're nice / How to use them</p>
         </div>
 
 
-        <div>
+        <div class="article">
             <a href="{base}/python_gc">Garbage collection in Python</a>
             <time datetime="2024-02-29">24.02.29</time>
             <p>Python has GC? / When? / Why? / What does this mean for app devs?</p>
@@ -37,7 +37,7 @@
 
 
         <h2 id="2023">2023</h2>
-        <div>
+        <div class="article">
             <a>Old post</a>
             <time datetime="2023-01-01">23.01.01</time>
             <p>In medias res</p>
@@ -93,30 +93,34 @@
 
 <style>
 
-.nav-text a {
-    text-decoration: none;
-    font-family: ui-monospace;
-    font-size: 16px;
+h2 {
+    @apply mb-2 text-xl font-semibold;
 }
 
-.article-border {
+.monospace a {
+    font-family: ui-monospace;
+}
+
+.border-l-1-w {
     border-left: 1px solid white;
 }
 
-a {
-    font-size: 20px;
+.article {
+    margin-bottom: 0.5rem;
+}
+
+.article p {
+    margin-top: 0.25rem;
+}
+
+.article a {
+    font-size: 1.125rem;
 }
 
 time {
     font-style: italic;
     font-size: 14px;
-}
-
-ul {
-  justify-content: flex-start;
-  list-style-type: none;
-  padding: 0px 0px 0px 0px;
-  width: 60px;
+    margin-left: 0.25rem;
 }
 
 </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,3 @@
 
-<h1 class="text-2xl"><a href="https://github.com/davidbrackbill/diction">Diction</a></h1>
+<h1><a href="https://github.com/davidbrackbill/diction">Diction</a></h1>
 <p>Query nested Python dictionaries using a GraphQL-like API.</p>
